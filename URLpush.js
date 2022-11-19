16bca8ce68af4ab3bcb14376989c4f01eb28111b
MrCocktail <jdavidbruno10@gmail.com>
2022-11-19 08:51:32 -0800

    Final

--- URLpush.js
+++ URLpush.js
@@ -2,25 +2,37 @@ const DEBUG = true
 const log = (value) => DEBUG && console.log(value)
 
 //- -\\
-let url_ = "?product=shirt&color=blue&newuser&size=m" //url mwen pral ajoute nan browser a touswit aprè domèn nan 
-window.history.pushState({}, '', url_) // mwen push li la
-let queryString = window.location.search; // sa se yon metòd ki pèmèt mwen rekipere tout paramèt yo san non domèn nan
-const full_url = new URL(window.location.href) // mwen jis met sa la pou n wè diferans lan; sa bay tout url la nèt(domèn + paramèt)
-let current_url = queryString, new_url
+let url = new URL(window.location.href)
+log(url.searchParams.toString()) 
+let object = {
+    project : "shirt",
+    color : "blue "
+}
+// let queryString = window.location.search; // sa se yon metòd ki pèmèt mwen rekipere tout paramèt yo san non domèn nan
+// const full_url = new URL(window.location.href) // mwen jis met sa la pou n wè diferans lan; sa bay tout url la nèt(domèn + paramèt)
+// let current_url = queryString
+// let new_url
 
 // console.log(queryString);
-let urlParams = new URLSearchParams(queryString);
+let urlParams = new URLSearchParams(object);
+window.history.pushState({}, '', ["?",urlParams].join(""))
+// log(window.location.search)
 
 /**Mwen pral kreye metòd pou ajoute {kle=valè} nan URL la
  * Li p ap kraze sa k te la deja yo !!!
  */
 const add = (search, value) => { 
-    new_url = current_url + `&${search}=${value}`
-    window.history.replaceState({}, '', new_url)
-    current_url = window.location.search 
+    search = encode(search)
+    value = encode(value)
+    // object[search] = value
+    // urlParams = new URLSearchParams(object);
+    urlParams.set(search, value)
+    window.history.replaceState({}, '', ["?",urlParams].join(""))
+    
 }
-
-let couple, getter
+add("size", "xl")
+add("author", "dave")
+add("location?", "here?")
 
 /**Mwen pral kreye metòd pou modifye yon kle nan URL la a kondisyon ke li egziste
  * Si nou just itilize metòd <.set> la, paj la p ap sispann reload,
@@ -30,19 +42,23 @@ const update = (search, value) => {
     urlParams = new URLSearchParams(window.location.search)
     if (urlParams.has(search)) {
         urlParams.set(search, value)
-        window.history.pushState({}, '', urlParams)
+        window.history.replaceState({}, '', ["?",urlParams].join(""))
     } else log("Can't update this key because it doesn't exist") // Bon la m pa konn si se yon eksepsyon n ap kreye oubyen n ap jis log
 }
 
-const
-    keys = urlParams.keys(),
-    values = urlParams.values(),
-    entries = urlParams.entries();
+update("size", "sm")
 
 //Pati get la
 
 const get = (search) => {
+    let couple, getter
+    let entries = urlParams.entries();
+    search = encodeURIComponent(search).replace(
+        /[!'()*]/g,
+        (c) => `%${c.charCodeAt(0).toString(16).toUpperCase()}`
+    )
     for (couple of entries) {
+        
         if (couple[0] == search) { 
             getter = `{${couple[0]}:"${couple[1]}"}` 
             break
@@ -50,26 +66,32 @@ const get = (search) => {
     }
     return getter
 }
-// log(get('color'))
+log(get('color'))
+log(get("size"))
+log(get("location?"))
 
 // Pati pou efase yon paramèt
 const remove = (search) => {
     if (urlParams.has(search)) {
         urlParams.delete(search)
-        window.history.replaceState({}, '', urlParams)
+        window.history.replaceState({}, '', ["?",urlParams].join(""))
     } 
     else log("Key not found")
 }
+// remove()
 
-log(add("boul", 2))
-log(add("laj", 20))
-// log(urlParams)
-log(update("laj", 18))
-// 
-log(window.location.search)
-/**Limit pwogram nan:
- * Pafwa mwen jwenn erè <Cannot GET> sou URL la. 
- * Sa se lè m ap fè on pakèt tès
- * Men depi m fèmen sèvè a, epi m reouvè l, tout bagay ok.
- * M panse sa s on pwoblèm kanmèm.
- */
+function encode(str) {
+    str = encodeURIComponent(str).replace(
+        /[!'()*]/g,
+        (c) => `%${c.charCodeAt(0).toString(16).toUpperCase()}`
+    )
+    return str
+}
+let o = encode("hello world &&*$%")
+// log(o)
+function decode(p) {
+    p.toString()
+    p = decodeURIComponent(p.replace(/\+/g, " "));
+    return p
+}
+// log(decode(o))
